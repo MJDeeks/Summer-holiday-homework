679573100c8c2810786b3286853dcb24c07ff7ac
18dbrennan-rasmussen <18dbrennan-rasmussen@mlmc.vic.edu.au>
2018-01-19 20:38:43 +1100

    Holiday homework task

---
+++ Part 2/DNA2.py
@@ -0,0 +1,29 @@
+#open all fragments
+fragment_1 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment1.txt')
+fragment_2 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment2.txt')
+fragment_3 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment3.txt')
+fragment_4 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment4.txt')
+fragment_5 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment5.txt')
+fragment_6 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/fragment6.txt')
+
+#read all fragments
+frag1 = fragment_1.read()
+frag2 = fragment_2.read()
+frag3
+frag4
+frag5
+frag6
+
+#open all suspects
+suspect_1 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/suspect1.txt')
+suspect_2 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/suspect2.txt')
+suspect_3 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/suspect3.txt')
+suspect_4 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/suspect4.txt')
+suspect_5 = open('/Users/18dbrennan-rasmussen/PycharmProjects/Summer-holiday-homework/Part 2/suspect5.txt')
+
+#read all suspects
+sus1
+sus2
+sus3
+sus4
+sus5
